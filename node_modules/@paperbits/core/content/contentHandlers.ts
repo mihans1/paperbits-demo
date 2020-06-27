6691de0dc3ec8e1836df0ac650334b607d53497f
Mikk Hanson <mihans@ttu.ee>
2020-06-27 12:19:01 +0300

    added installation

---
+++ node_modules/@paperbits/core/content/contentHandlers.ts
@@ -0,0 +1,37 @@
+import { DragSession } from "@paperbits/common/ui/draggables";
+import { ViewManager, IContextCommandSet } from "@paperbits/common/ui";
+import { IWidgetHandler, WidgetContext } from "@paperbits/common/editing";
+import { WidgetModel } from "@paperbits/common/widgets";
+
+
+export class ContentHandlers implements IWidgetHandler {
+    constructor(private readonly viewManager: ViewManager) { }
+
+    public canAccept(dragSession: DragSession): boolean {
+        return dragSession.sourceBinding.name === "section";
+    }
+
+    public getContextualEditor(context: WidgetContext): IContextCommandSet {
+        const contextualEditor: IContextCommandSet = {};
+
+        if (context.model.widgets.length === 0) {
+            contextualEditor.hoverCommand = {
+                color: "#2b87da",
+                position: "center",
+                tooltip: "Add section",
+                component: {
+                    name: "grid-layout-selector",
+                    params: {
+                        onSelect: (model: WidgetModel) => {
+                            context.model.widgets.push(model);
+                            context.binding.applyChanges();
+                            this.viewManager.clearContextualEditors();
+                        }
+                    }
+                }
+            };
+        }
+
+        return contextualEditor;
+    }
+}
---
+++ node_modules/@paperbits/core/publishing/index.ts
@@ -0,0 +1,4 @@
+export * from "./localStyleBuilder";
+export * from "./mediaPublisher";
+export * from "./assetPublisher";
+export * from "./knockoutHtmlPagePublisherPlugin";
